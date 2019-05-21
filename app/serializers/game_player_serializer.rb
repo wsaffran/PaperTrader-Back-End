eff5da06e0cb4c643b4c9a5ac49bc8a858850002
github username <github email address>
2019-05-21 12:53:28 -0400

    tweaks

--- app/serializers/game_player_serializer.rb
+++ app/serializers/game_player_serializer.rb
@@ -1,15 +1,15 @@
 class GamePlayerSerializer < ActiveModel::Serializer
   attributes :id, :game, :user, :cash_balance, :transactions
 
-  belongs_to :game
-  belongs_to :user
+  belongs_to :game, serializer: GameSerializer
+  belongs_to :user, serializer: UserSerializer
   has_many :transactions, serializer: TransactionSerializer
 
-  def transactions
-    self.object.transactions.map do |transaction|
-      {id: transaction.id}
-    end
-  end
+  # def transactions
+  #   self.object.transactions.map do |transaction|
+  #     {id: transaction.id}
+  #   end
+  # end
 
   # def game
   #   self.object.game do |game|
